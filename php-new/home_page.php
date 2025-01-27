14c6a7226e00d3d8c5c5214d09341ad0749bad17
jeff <jeffy7563@gmail.com>
2025-01-27 10:16:39 +0700

    Add files via upload

--- php-new/home_page.php
+++ php-new/home_page.php
@@ -205,6 +205,6 @@ $conn->close();
         </footer>
 
         <script src="https://unpkg.com/scrollreveal"></script>
-        <script src="./js/main.js"></script>
+        <script src="../js/main.js"></script>
 </body>
 </html>
